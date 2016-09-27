5600cb595e2d929d6e09908366059b2b9766ec7f
Igor <igor.pogosian@small-team.com>
2016-09-27 17:15:58 +0300

    add task fixed

--- app/assets/javascripts/app/controllers/tasks_controller.js
+++ app/assets/javascripts/app/controllers/tasks_controller.js
@@ -5,6 +5,7 @@ app.controller('TasksController', ['$scope', '$http', '$auth', 'Flash', TasksCon
 
 function TasksController($scope, $http, $auth, Flash){
     $scope.add_task = function(){
+        if(!validate()) return Flash.create('danger', 'Title can\'t be empty!');
         $http({
             method: 'POST',
             url: $auth.apiUrl() + '/projects/'+ $scope.project.id + '/tasks',
@@ -16,5 +17,9 @@ function TasksController($scope, $http, $auth, Flash){
         }, function errorCallback() {
             Flash.create('danger', 'Not authorized!');
         });
+    };
+
+    function validate(){
+        return $scope.task_title != null && $scope.task_title.length > 0
     }
 }
