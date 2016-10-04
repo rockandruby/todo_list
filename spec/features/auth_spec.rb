f32ec19a82c702a5a723632e34ecb371c5d2e71e
Igor <igor.pogosian@small-team.com>
2016-10-04 21:16:41 +0300

    fixed auth feature bugs

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -7,26 +7,30 @@ RSpec.feature "Auth", type: :feature do
 
   let(:user) { create(:user) }
 
-  # scenario 'Sign up user', :js do
-  #   reg_user
-  #   expect(page).to have_content 'Sign out'
-  # end
+  scenario 'Sign up user', :js do
+    reg_user
+    expect(page).to have_content 'Sign out'
+  end
 
   scenario 'Sign in user', :js do
     login_user(user)
     expect(page).to have_content 'Sign out'
   end
-  #
-  # scenario 'Facebook auth', :js do
-  #   click_on 'Sign in'
-  #   click_on 'Sign in with Facebook'
-  #   expect(page).to have_content 'Sign out'
-  # end
-  #
-  # scenario 'Sign out user', :js do
-  #   reg_user
-  #   click_on 'Sign out'
-  #   expect(page).to have_content 'Sign in'
-  # end
+
+  scenario 'Facebook auth', :js do
+    click_on 'Sign in'
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] =
+        OmniAuth::AuthHash.new({info: {email: 'email@mail.com', name: FFaker::Name.name},
+                                uid: '1234', provider: 'facebook'})
+    click_on 'Sign in with Facebook'
+    expect(page).to have_content 'Sign out'
+  end
+
+  scenario 'Sign out user', :js do
+    login_user(user)
+    click_on 'Sign out'
+    expect(page).to have_content 'Sign in'
+  end
 
 end
