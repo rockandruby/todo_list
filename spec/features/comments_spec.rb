e4bc9b43eec76b985f361254304805b8eacb58df
Igor <igor.pogosian@small-team.com>
2016-10-05 14:30:37 +0300

    fixed comments feature bugs

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -4,7 +4,8 @@ RSpec.feature "Comments", type: :feature do
   background do
     visit '/'
     login_user(user)
-    create(:project, user: user)
+    project = create(:project, user: user)
+    create_list(:comment, 5, task: project.tasks.take)
   end
 
   given(:user) { create(:user) }
@@ -16,20 +17,14 @@ RSpec.feature "Comments", type: :feature do
       fill_in 'comment_body', with: FFaker::Lorem.sentence
       find('#exampleInputFile', visible: false).set(Rails.root + '/spec/support/test_file.txt')
     end
-    expect { click_on 'Add comment' }.to change{sleep 1; find_all('.md-3-line').count}.by(1)
+    expect { click_on 'Add comment' }.to change { sleep 1; find_all('.md-3-line').count }.by(1)
   end
 
-  # scenario 'Delete comment' do
-  #   within ('.project') do
-  #     within find(:css, '.task', match: :first) do
-  #       find('md-ink-ripple').click
-  #       find('add_comment').click
-  #       within '#comments_modal' do
-  #         expect do
-  #           find(:css, '.md-3-line', match: :first).find('delete_comment').click
-  #         end.change(find('.md-3-line').length).by(-1)
-  #       end
-  #     end
-  #   end
-  # end
+  scenario 'Delete comment' do
+    manage_instruments
+    find(:css, '.md-open-menu-container .add_comment', match: :first).click
+    expect do
+      find(:css, '.delete_comment', match: :first).click
+    end.to change { sleep 1; find_all('.md-3-line').length }.by(-1)
+  end
 end
