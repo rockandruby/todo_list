1b6d656ced3874a7b78aa2d1e465f56d16920bce
Igor <igor.pogosian@small-team.com>
2016-10-06 11:01:21 +0300

    refac finished

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,7 @@
 //= require app/router
 //= require_tree ./app/services
 //= require_tree ./app/controllers
+//= require_tree ./app/directives
 //= require angular-rails-templates
 //= require_tree ./app/views
 //= require_tree .
